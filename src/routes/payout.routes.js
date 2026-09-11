820f96d4fa4c484211e870599b3ed19ec9effbaa
0000aditya0000 <adityagangwar516@gmail.com>
2026-09-11 21:46:34 +0530

    payout secert

--- src/routes/payout.routes.js
+++ src/routes/payout.routes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const payoutController = require('../controllers/payout.controller');
 const { validate } = require('../middlewares/validation');
 const { payoutCreateSchema } = require('../validators/hzpay.validator');
+const { requirePayoutSecret } = require('../middlewares/requirePayoutSecret');
 
 const router = express.Router();
 
-router.post('/create', validate(payoutCreateSchema), payoutController.createPayout);
+router.post('/create', requirePayoutSecret, validate(payoutCreateSchema), payoutController.createPayout);
 router.post('/webhook', payoutController.payoutWebhook);
 
 module.exports = router;
